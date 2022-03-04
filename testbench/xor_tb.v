52c7210d0c7476adeea422ba900265475565fa4f
Erick <ahmederick00@gmail.com>
2022-03-04 01:38:43 +0100

    Create xor_tb.v

---
+++ testbench/xor_tb.v
@@ -0,0 +1,18 @@
+'include "predefined/xor_gate.v"
+
+module nor_tb();
+    reg ta, tb;
+    wire ty;
+
+    xor_gate dut(y, ta, tb);
+
+    initial begin
+        ta=0, tb=0;
+        $monitor("IN: %b, %b ",ta, tb, "OUT: ",y);
+        ta=0, tb=1;
+        $monitor("IN: %b, %b ",ta, tb, "OUT: ",y);
+        ta=1, tb=0;
+        $monitor("IN: %b, %b ",ta, tb, "OUT: ",y);
+        ta=1, tb=1;
+    end
+endmodule
