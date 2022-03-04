fa27547a5061c8a40611c7d3742511f4fc4863cb
Erick <ahmederick00@gmail.com>
2022-03-05 00:50:03 +0100

    performance optimizations
    added carry lookahead pakage used inside adder16

---
+++ cpu/alu/lookahead.sv
@@ -0,0 +1,17 @@
+package lookahead (
+    output [15:0]carry_bit,
+    input [15:0]a,
+    input [15:0]b,
+);
+
+    res = and16(y, a, b);
+
+    for (i=0; i<15 ; i=i+1)
+        if (res[i] = 0)
+            carry_bit[i] = 0;
+        else
+            carry_bit[i] = 1;
+        end
+    end
+
+endpackage
