09676ffb82d03aabe13473abad99b5c3b0d062fd
Erick <ahmederick00@gmail.com>
2022-03-27 19:00:14 +0200

    minor fixes

--- cpu/cpu.sv
+++ cpu/cpu.sv
@@ -4,27 +4,31 @@ import reg16::*
 
 'include "gates/mux16.v"
 
-package cpu (
+module cpu (
     input [15:0]inM,
     input [15:0]instr,
     input reset,
     input clk,
-    input sel[15:0]
     output [15:0]outM,
-    output flagM[2:0]
+    output [2:0]flagM
     output wrtM,
     output [15:0]addrM,
     output [15:0]pc
 );
 
+wire [11:6] [5:0] addrM = sel;
+
+//FIXME: wrtM is not defined!
+not (wrtM, notWrtM); //TODO: verify this
+
 mux16 instrMux(outInstrMux, outM, instr, wrtM);
-reg16 AReg(outInstrMux, wrtM, clk, addrM notOutAReg);
+reg16 AReg(outInstrMux, wrtM, clk, addrM, notOutAReg);
 
 mux16 inputMux(InAlu1, addrM, inM, wrtM);
-reg16 DReg(outM, wrtM, clk, InAlu0, notOut);
+reg16 DReg(outM, notWrtM, clk, InAlu0, notOut);
 
 alu alu(InAlu0, InAlu1, sel, outM, flagM);
 
 pc pc(addrM, wrtM, reset, clk, pc);
 
-endpackage
+endmodule
